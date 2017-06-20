b57e0cc38507279dd2ec02fc9ba8e86899c8df79
paulnheera <paul.nheera@gmail.com>
2017-06-20 23:16:39 +0200

    First version of the Bollinger Bands Backtest.

---
+++ BBands_Strategy.R
@@ -0,0 +1,76 @@
+##Bollinger Bands:
+
+library(quantmod)
+library(PerformanceAnalytics)
+
+ticker = "JSE:STX40"
+getSymbols.google(ticker, env=globalenv())
+
+px = get(ticker) 
+
+portfolio = px
+portfolio[,] = NA
+portfolio = portfolio[,1:3]
+colnames(portfolio) = c("Cash","Equity","Total")
+
+no.Shares = px[,1]
+no.Shares[,] =  0
+colnames(no.Shares) = ticker
+
+#Calculate Bollinger Bands:
+Bbands = BBands(px[,4],n=20,maType = "SMA", sd=2)
+
+startpos = 21
+portfolio[startpos-1,"Cash"] = 10000
+portfolio[startpos-1,"Equity"] = 0
+portfolio[startpos-1,"Total"] = sum(portfolio[startpos-1,1:2])
+
+
+
+
+#The Loop - Go Through Each Bar:
+for(i in (startpos+1):nrow(px)){ #(Currently with one day lag in execution.)
+  
+  #carry over cash:
+  portfolio[i,"Cash"] = portfolio[i-1,"Cash"]
+  no.Shares[i] = no.Shares[i-1]
+  
+  #Check current positions 
+  if(no.Shares[i] == 0){
+    #Check for entry signal if = 0 shares:
+    if(Bbands[i-1,"pctB"] < 0){
+      #Buy Shares:
+      no.Shares[i] = as.integer(portfolio[i,"Cash"]/px[i,4]) #NOTE: Change the 4 to Close.
+      portfolio[i,"Equity"] = no.Shares[i]*px[i,4]
+      portfolio[i,"Cash"] = portfolio[i,"Cash"] - portfolio[i,"Equity"]
+      portfolio[i,"Total"] = portfolio[i,"Equity"] + portfolio[i,"Cash"]
+    }else{
+      no.Shares[i] = no.Shares[i-1]
+      portfolio[i,"Equity"] = no.Shares[i]*px[i,4]
+      portfolio[i,"Total"] = portfolio[i,"Equity"] + portfolio[i,"Cash"]
+    }
+      
+  }else{
+    #Check for exit signal if > 0 shares:
+    if(Bbands[i-1,"pctB"] > 1){
+      #Sell Shares:
+      portfolio[i,"Equity"] = no.Shares[i]*px[i,4]
+      portfolio[i,"Cash"] = portfolio[i,"Cash"] + portfolio[i,"Equity"]
+      portfolio[i,"Equity"] = 0
+      portfolio[i,"Total"] = portfolio[i,"Equity"] + portfolio[i,"Cash"]
+      no.Shares[i] = 0
+    }else{
+      no.Shares[i] = no.Shares[i-1]
+      portfolio[i,"Equity"] = no.Shares[i]*px[i,4]
+      portfolio[i,"Total"] = portfolio[i,"Equity"] + portfolio[i,"Cash"]
+    }
+  }
+  #Move on to the next bar:
+}
+
+startDate = "2007-01-29"
+hold.ret = ROC(px[paste0(startDate,"::"),4])
+strat.ret = ROC(portfolio[paste0(startDate,"::"),"Total"])
+
+ret = merge.xts(hold.ret,strat.ret)
+chart.CumReturns(ret,col=c("green","blue"))
