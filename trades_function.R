13d6af40566b3a8ffa8820ecd1cd1919e6113032
paulnheera <paul.nheera@gmail.com>
2018-08-25 21:03:41 +0200

    Added trades function.

---
+++ trades_function.R
@@ -0,0 +1,18 @@
+#******************
+# Identify Trades :
+#******************
+
+get_trades <- function(position){
+  
+  temp <- (position != lag(position))
+  temp <- index(position)[which(temp==TRUE)]
+  
+  start <- temp[seq(from = 1,length(temp),by=2)]
+  end <- temp[seq(from = 2,length(temp),by=2)]
+  
+  trades <- data.frame(start=start,end=end,position = position[start])
+  
+  return(trades)
+}
+
+
