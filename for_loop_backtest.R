712fb2496d192e60cf9d2e9d998be20694ba05bd
paulnheera <paul.nheera@gmail.com>
2018-08-25 21:04:32 +0200

    Arbitary changes on for_loop_backtest

--- for_loop_backtest.R
+++ for_loop_backtest.R
@@ -16,6 +16,7 @@ library(ggplot2)
 library(scales)
 library(readr)
 source('getSymbol_MT4.R')
+source('trades_function.R')
 
 
 #---- Market Data ----
@@ -30,48 +31,45 @@ fastSMA = SMA(Cl(ts),14)
 slowSMA = SMA(Cl(ts),50)
 
 #Bollinger Bands:
-bbands = BBands(Cl(ts),20,sd=1.5)
+bbands = BBands(Cl(ts),12,sd=2)
 bbands = cbind(Cl(ts),bbands)
 
-majorSMA = SMA(Cl(ts),200)
 
-trade_filter = NA
-
+#---- Inittialize Trade info Objects ----
 position = Cl(ts); position[,] = 0 ;colnames(position) = "position"
+
+#Take Profit and Stop Loss:
 stop_loss = position; stop_loss[,] = NA
 take_profit = stop_loss
 
 exit_to_enter = FALSE  #TRUE if short exit conditions are the same as long enrty conditions and vice versa.
 
-
-long_entry_cond = "(fastSMA[i] > slowSMA[i]) && (fastSMA[i-1] < slowSMA[i-1])"
-short_entry_cond = "(fastSMA[i] < slowSMA[i]) && (fastSMA[i-1] > slowSMA[i-1])"
-
+#---- Set Filters ----
 long_filter =  Cl(ts); long_filter[,] = TRUE 
-long_filter <- SMA(Cl(ts),200) < SMA(Cl(ts),100)
-short_filter <- SMA(Cl(ts),200) > SMA(Cl(ts),100)
-short_filter =  Cl(ts); short_filter[,] = TRUE#SMA(Cl(ts),200) > SMA(Cl(ts),100)
+short_filter =  Cl(ts); short_filter[,] = TRUE
 
+long_filter <- SMA(Cl(ts),100) < SMA(Cl(ts),50)
+short_filter <- SMA(Cl(ts),100) > SMA(Cl(ts),50)
+
+n = 100
 #---- Backtest ----
 
-for_loop_backtest <- function(OHLC="",Indicator=""){
-  
-  #ts = OHLC
-  #bbands = Indicator
+#for_loop_backtest <- function(OHLC="",Indicator=""){
   
   for(i in (n+1):nrow(ts)){
   
   #---- Check for Open:----
   if(position[i-1] == 0){ #If there was no position open:
     
-    #Strategy rule: Open Long
+    #Strategy rule: Open Long!
     if((bbands[i,'dn'] > Cl(ts)[i]) && (bbands[i-1,'dn'] < Cl(ts)[i-1]) && long_filter[i]){
       position[i] = 1
       stop_loss[i] = NA
       take_profit[i] = NA  
       ##IMPROVE: Make the 3 Statements a method that can be run when closing a short position.
+      ##This would be for the case where conditions for opening short are the same as closing long.
                         
-    #Strategy rule: Open Short  
+    #Strategy rule: Open Short! 
     }else if((bbands[i,'up'] < Cl(ts)[i]) && (bbands[i-1,'up'] > Cl(ts)[i-1]) && short_filter[i]){
       ##IMPROVE: (TEST SENSITIVITY) fastSMA[i] < slowSMA[i] && lag(fastSMA,1)[i] > lag(fastSMA,1)[i]
       ##REASONING: This allows flexibility in the amount of variables checked in conditions.
@@ -171,19 +169,28 @@ for_loop_backtest <- function(OHLC="",Indicator=""){
     
   }
   
+  #Return Stream:
   ret = ROC(Cl(ts))
   
-  strat_ret = ret*lag(-position) 
-
+  #Strategy Returns:
+  strat_ret = ret*lag(position) 
+  
+  #Table of summary stats:
   table.AnnualizedReturns(strat_ret)[3,]
-}
+  
+#Return: Positions & Strategy Returns
 
+#}
+
+  
+#---- Trades ----
+trades <- get_trades(position)
 
 #---- Visualization: Positions ----
 
 #Subset Period:
-from = "2017-01-01"
-to = "2017-12-31"
+from = "2018-01-01"
+to = "2018-12-31"
 
 start_ind <-  min(which(trades$start > from))
 end_ind <- max(which(trades$start < to))
@@ -192,7 +199,6 @@ end_ind <- max(which(trades$start < to))
 g_ts <- ts[paste0(from,"::",to)]
 gdata <- data.frame(Time = as.POSIXct(index(g_ts)),g_ts)
 
-
 #Plot Graph:
 ggplot() +
   geom_rect(data = trades[start_ind:end_ind,], aes(xmin = start, xmax = end, 
@@ -200,9 +206,12 @@ ggplot() +
             fill = ifelse(trades[start_ind:end_ind,'position'] < 0,'red','green'),
             alpha = 0.2) +
   geom_line(data=gdata,aes(x=Time,y=Close),size=0.8,colour='blue')
+
+#---- Visualization: With Indicators ----
   
 #---- Visualization: Performance ----
 strat_cumret <- cumprod(1+na.trim(strat_ret)) - 1
+
 drawdowns <- Drawdowns(strat_ret)
 
 gdata <- data.frame(Time = index(strat_cumret),strat_cumret)
@@ -214,42 +223,6 @@ ggplot(gdata,aes(x=Time,y=Close)) +
   xlab('Date')
 
 #---- Optimization ----
-N <- seq(5,100,by=5)
-
-sharpe_ratios <- data.frame(look_back = N,`Sharpe Ratio`=NA,
-                            check.names = FALSE)
-
-for(n in N){
-  bbands = BBands(Cl(ts),n)
-  
-  for_loop_backtest()
-  
-  sharpe_ratios[which(N == n),2] = for_loop_backtest()
-  
-}
-
-#Plot Positions:
-plot(position,type='l')
-
-pos_df = data.frame(Date = index(position),Position = position,check.names = FALSE)
-colnames(pos_df) =c('Date','Position')
-
-ggplot(pos_df,aes(x=as.Date(Date),y=Position)) +
-  geom_bar(stat='identity') +
-  scale_x_date(labels=date_format("%b-%Y"))
-  
 
 
-#Performance:
-ret = ROC(Cl(ts))
-adj_ret = ROC(Ad(ts))
 
-hist(ret,breaks=100)
-hist(adj_ret,breaks = 100)
-
-strat_ret = ret*lag(position) 
-
-hist(strat_ret,breaks=100)
-hist(adj_ret,breaks = 100)
-
-charts.PerformanceSummary(strat_ret)
